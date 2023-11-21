b11c968f7272b7a62b86572c3311f27c5fc70679
alelizarazu <alejandrolizarazu3@gmail.com>
2023-11-21 20:01:44 -0300

    segundo commit

--- tp.js
+++ tp.js
@@ -1,2 +1,14 @@
-let num1;
-let num2;
+let nombre ;
+let servicio;
+let servicio1;
+let servicio2;
+
+
+nombre= prompt("Hola bienvenido ingrese su nombre");
+servicio= prompt ("hola " + nombre + " seleccione uno de los siguientes servicios (use numeros): 1) edicion de videos 2 maquetado web 3 redaccion periodistica")
+if (servicio == 1) {prompt("seleccione una de las opciones(use numeros): 1) video de 30s a 1min 2) video de 1min a 5min 3) video de 5 min a 15m ")}
+    else {if (servicio == 2) {prompt ("seleccione una de las opciones(use numeros): 1) maquetado pagina web 30$")} 
+        else{if (servicio == 3) {prompt ("seleccione una de las opciones(use numeros): 1)")}
+            else {alert('ingreso una opcion no valida')}}}
+
+console.log ("gracias por su compra")
